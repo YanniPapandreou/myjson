475d526117b0b7656b9807a8a4fdd953c1095602
Yanni Papandreou <nepo1y@gmail.com>
2026-01-06 21:32:49 +0000

    added more messages

--- src/root.zig
+++ src/root.zig
@@ -2,6 +2,6 @@ const std = @import("std");
 
 pub fn parse(T: type, src: []const u8) !T {
     _ = src;
-    std.debug.print("TODO: parse json\n", .{});
+    std.debug.print("TODO: parse json and add more features\n", .{});
     return undefined; // don't do this, for demonstration purposes only
 }
